6058e9e3c605f4ce0652b03a7257fdb5ca2f0490
MunirajaReddyB <bmunirajareddy582@gmail.com>
2021-02-24 15:30:28 +0530

    Annotations and retreving db records using hibernate get method

--- Hibernate_Tutorial/src/main/java/org/muniraja/dto/UserDetails.java
+++ Hibernate_Tutorial/src/main/java/org/muniraja/dto/UserDetails.java
@@ -1,14 +1,24 @@
 package org.muniraja.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
-@Entity
+//@Entity annotation tells to the hibernate this the type of object which needs to be persist.
+//@id define this particular property is the primary key for the table
+//@Column tells to the hibernate use this name as column name instead of property name
+//@Transient annotation tells to hibernate this particular property is not mandatory
+@Entity (name = "USER_DETAILS")
 public class UserDetails {
 
     @Id
+    @Column (name = "USER_ID")
     private int userId;
+    @Column (name = "USER_NAME")
     private String userName;
+    //@Transient
+    private String vehicletype;
 
     public int getUserId() {
         return userId;
@@ -24,6 +34,14 @@ public class UserDetails {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public String getVehicletype() {
+        return vehicletype;
+    }
+
+    public void setVehicletype(String vehicletype) {
+        this.vehicletype = vehicletype;
     }
 
 }
--- Hibernate_Tutorial/src/main/java/org/muniraja/hibernateTest/HibernateTest.java
+++ Hibernate_Tutorial/src/main/java/org/muniraja/hibernateTest/HibernateTest.java
@@ -5,22 +5,39 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.muniraja.dto.UserDetails;
 
+import java.sql.SQLOutput;
+
 public class HibernateTest {
 
     public static void main(String[] args){
 
         UserDetails user = new UserDetails();
         user.setUserId(10);
-        user.setUserName("Muniraja");
+        user.setUserName("Vijay Devarakonda");
 
-        //Configuration().configure() reads hibernate.cfg.xml file and create one sf object per application
+        UserDetails user1 = new UserDetails();
+        user1.setUserId(12);
+        user1.setUserName("Vijay Sethupathi");
+
+        //Configuration().configure() reads hibernate.cfg.xml file and creates one sf object per application
         //sf will have factory of session objects and all the transactions will happens through session
         //using session we will run crud operations on the database.
         SessionFactory sf = new Configuration().configure().buildSessionFactory();
         Session session = sf.openSession();
         session.beginTransaction();
         session.save(user);
+        session.save(user1);
         session.getTransaction().commit();
+        session.close();
+
+        UserDetails userDetails = null;
+        //retrieving objects from db using hibernate get() method
+        Session session1 = sf.openSession();
+        session1.beginTransaction();
+        userDetails = (UserDetails) session1.get(UserDetails.class,12);
+        System.out.println("UserId:"+userDetails.getUserId());
+        System.out.println("UserName:"+userDetails.getUserName());
+
     }
 
 }
